9eaebf497e3032cc82158b0955f459421d5e2c20
C1n3k99 <48324489+C1n3k99@users.noreply.github.com>
2021-01-30 00:23:39 +0100

    Added serwer folder

---
+++ serwer/serwer.c
@@ -0,0 +1,87 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+struct thread_data_t
+{
+    int my_socket;
+};
+
+void *ThreadBehavior(void *t_data)
+{ 
+    pthread_detach(pthread_self());
+    struct thread_data_t *th_data = (struct thread_data_t*)t_data;
+    int desc = th_data->my_socket;
+    //write(desc, "HI", 2);
+    //char* buf=malloc(10*sizeof(char));
+    //read(desc, buf, 10);
+}
+
+void handleConnection(int connection_socket_descriptor)
+{
+    int create_result = 0;
+    pthread_t thread1;
+
+    struct thread_data_t* t_data = malloc(sizeof(struct thread_data_t));
+    t_data->my_socket = connection_socket_descriptor;
+
+    create_result = pthread_create(&thread1, NULL, ThreadBehavior, (void *)t_data);
+    if (create_result){
+       printf("Błąd przy próbie utworzenia wątku, kod błędu: %d\n", create_result);
+       exit(-1);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    struct sockaddr_in server_address;
+
+    memset(&server_address, 0, sizeof(struct sockaddr));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(atoi(argv[1]));
+
+    int server_socket_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket_descriptor < 0)
+    {
+        fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda..\n", argv[0]);
+        exit(1);
+    }
+    char reuse_addr_val = 1;
+    setsockopt(server_socket_descriptor, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse_addr_val, sizeof(reuse_addr_val));
+
+    int bind_result = bind(server_socket_descriptor, (struct sockaddr*)&server_address, sizeof(struct sockaddr));
+    if (bind_result < 0)
+    {
+        fprintf(stderr, "%s: Błąd przy próbie dowiązania adresu IP i numeru portu do gniazda.\n", argv[0]);
+        exit(1);
+    }
+
+    int listen_result = listen(server_socket_descriptor, 4);
+    if (listen_result < 0) {
+        fprintf(stderr, "%s: Błąd przy próbie ustawienia wielkości kolejki.\n", argv[0]);
+        exit(1);
+    }
+    
+    int connection_socket_descriptor;
+
+    while(1)
+    {
+        connection_socket_descriptor = accept(server_socket_descriptor, NULL, NULL);
+        if (connection_socket_descriptor < 0)
+        {
+            fprintf(stderr, "%s: Błąd przy próbie utworzenia gniazda dla połączenia.\n", argv[0]);
+            exit(1);
+        }
+        handleConnection(connection_socket_descriptor);
+    }
+}
