80fe90ce4cd337042e4bac861da3a0772a1978f0
BlackRoosie <73405676+BlackRoosie@users.noreply.github.com>
2021-01-30 15:28:59 +0100

    try to load qss file and tcpSocket

--- klient/mainwindow.cpp
+++ klient/mainwindow.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):
+      QMainWindow(parent),
+      ui(new Ui::MainWindow),
+      tcpSocket(new QTcpSocket(this))
 {
     ui->setupUi(this);
     //ui->leftCardValue->setStyleSheet("QLabel {  color : white; }");
@@ -17,7 +19,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_connect_clicked()
 {
-
+    tcpSocket->connectToHost("127.0.0.1", 1234);
 }
 
 void MainWindow::on_exitGame_clicked()
