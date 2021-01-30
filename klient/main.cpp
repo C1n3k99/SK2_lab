869a4d2d6d97db87a6aea234947bf070fbd3bcdf
BlackRoosie <73405676+BlackRoosie@users.noreply.github.com>
2021-01-30 15:27:52 +0100

    try to load qss file

--- klient/main.cpp
+++ klient/main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile file(":/Fibers.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+    a.setStyleSheet(styleSheet);
     MainWindow w;
     w.show();
     return a.exec();
