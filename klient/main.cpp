b720f4cc6088228dec7f08746ffd02ed75120b3d
BlackRose <dariasport@vp.pl>
2021-01-31 17:45:18 +0100

    no more warnings

--- klient/main.cpp
+++ klient/main.cpp
@@ -5,11 +5,16 @@
 
 int main(int argc, char *argv[])
 {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // DPI support
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps); //HiDPI pixmaps
+    qputenv("QT_SCALE_FACTOR", "1");
+
     QApplication a(argc, argv);
     QFile file("../klient/Fibers.qss");
     file.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(file.readAll());
     a.setStyleSheet(styleSheet);
+
     MainWindow w;
     w.show();
     return a.exec();
